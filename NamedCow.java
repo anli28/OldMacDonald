74735935068e3fbe55ab30b6232d33d473d5739c
Andy Li <andyli@Andys-MacBook-Pro.local>
2018-10-24 07:53:13 -0700

    Final

--- NamedCow.java
+++ NamedCow.java
@@ -1,8 +1,6 @@
 class NamedCow extends Cow
 {
-private String myType;
 private String myName;
-private String mySound;
 
 public NamedCow(String type, String name, String sound){
 	myType = type;
@@ -14,7 +12,6 @@ public NamedCow(){
 	myName = "unknown";
 	mySound = "unknown";
 }
-	public String getSound(){return mySound;}     
-    public String getType(){return myType;} 
+	 
     public String getName(){return myName;}
 }
